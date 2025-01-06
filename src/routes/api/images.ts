1b8c0d4e02c05ef759378d42206d1d468edf54d7
Pedro Leal <lealpedro2009@gmail.com>
2025-01-06 21:59:41 +0000

    feat: images endpoint

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import path from "path";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Images route");
+  console.log(req.query);
+
+
+  const filePath = path.join(__dirname, `../../assets/full/${req.query.filename}.jpg`);
+
+  // send image
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send("Image not found");
+    }
+  } );
+
+
+  //res.send("Images route");
 });
 
 export default router;
